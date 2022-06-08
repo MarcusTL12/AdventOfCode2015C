c5cbfdf13eb5237a1d56790ed9eb29ec739aea99
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-06-08 20:41:02 +0200

    Day 1

--- src/day1.c
+++ src/day1.c
@@ -1,5 +1,33 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void d1p1() { printf("Heiann!!\n"); }
+#include "aoc_util/file_util.h"
 
-void d1p2() {}
+void d1p1() {
+    char *inp = file_read_full("input/day1/input");
+
+    int64_t floor = 0;
+    for (char *c = inp; *c; c++) floor += *c == '(' ? 1 : -1;
+
+    printf("%li\n", floor);
+
+    free(inp);
+}
+
+void d1p2() {
+    char *inp = file_read_full("input/day1/input");
+
+    int64_t floor = 0, ans;
+    for (char *c = inp; *c; c++) {
+        floor += *c == '(' ? 1 : -1;
+        if (floor == -1) {
+            ans = c - inp + 1;
+            break;
+        }
+    }
+
+    printf("%li\n", ans);
+
+    free(inp);
+}
